8436d04f6f666b15def7aa9d7a8773693ece0014
DawnShaker <danila2002kukil@gmail.com>
2025-02-25 11:55:40 -0600

    Rules fix 1.1

--- src/components/navigation/config/navigationLinks.ts
+++ src/components/navigation/config/navigationLinks.ts
@@ -3,14 +3,13 @@ export const rulesLinks = [
   { href: '/general-rules', label: 'Общие правила' },
   { href: '/heroes-league-errata', label: 'Эррата Лиги Героев' },
   { href: '/social-injustice', label: 'Социальная НЕ-справедливость' },
-  { href: '/house-rules', label: 'House Rules' },
-  { href: '/combat', label: 'Combat Rules' },
-  { href: '/magic', label: 'Magic & Spells' },
+  //{ href: '/house-rules', label: 'House Rules' },
+  //{ href: '/combat', label: 'Combat Rules' },
   { href: '/rules/downtime-activities', label: 'Активности простоя' }
 ];
 
 export const characterLinks = [
-  { href: '/character-guide', label: 'Создание персонажа' }, // Updated from /character-creation
+  { href: '/character-guide', label: 'Создание персонажа' },
   { href: '/classes', label: 'Классы' },
   { href: '/races', label: 'Виды' },
   { href: '/backgrounds', label: 'Предыстории' },
@@ -19,8 +18,8 @@ export const characterLinks = [
 
 export const toolsLinks = [
   { href: '/tools/point-buy', label: 'Point Buy калькулятор' },
-  { href: '/tools/dice-roller', label: 'Бросок костей' },
-  { href: '/tools/name-generator', label: 'Генератор имён' },
+  //{ href: '/tools/dice-roller', label: 'Бросок костей' },
+  //{ href: '/tools/name-generator', label: 'Генератор имён' },
   { href: '/tools/encounter-builder', label: 'Генератор столкновений' }
 ];
 
@@ -32,7 +31,8 @@ export const guideLinks = [
 
 export const handbookLinks = [
   { href: '/sigil', label: 'Сигил - город дверей' },
-  { href: '/magic-items', label: 'Магические предметы' },
+  //{ href: '/magic-items', label: 'Магические предметы' },
   { href: '/faq', label: 'FAQ' },
-  { href: '/third-party', label: 'Сторонний материал' }
+  //{ href: '/third-party', label: 'Сторонний материал' },
+  //{ href: '/magic', label: 'Magic & Spells' },
 ];
