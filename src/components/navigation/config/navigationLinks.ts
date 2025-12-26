c1251ee85c9177cb0705014ad01bfd7ce8b29a65
DawnShaker <danila2002kukil@gmail.com>
2025-12-26 13:40:50 +0200

    Rules fix 2.9.992a

--- src/components/navigation/config/navigationLinks.ts
+++ src/components/navigation/config/navigationLinks.ts
@@ -33,7 +33,7 @@ export const guideLinks = [
 
 export const handbookLinks = [
   { href: '/sigil', label: 'Сигил - город дверей' },
-  { href: '/glossary', label: 'Глоссарий' },
+  { href: '/glossary', label: 'Сас' },
   { href: '/items', label: 'Предметы' },
   { href: '/magic-items', label: 'Магические предметы' },
   { href: '/spells', label: 'Заклинания' },
