d73ff00ba9c7b031866b0b05ce635344f984509b
zzzRobottzzz <artem190201@gmail.com>
2026-08-08 15:54:21 +0700

    релиз попытка 2

--- public/downloads/league-roll20-sync/options.js
+++
@@ -1,4 +0,0 @@
-chrome.storage.local.get('leagueCabinetUrl').then(({ leagueCabinetUrl }) => {
-  if (!leagueCabinetUrl) return;
-  document.querySelector('#league-cabinet-link').href = leagueCabinetUrl;
-});
